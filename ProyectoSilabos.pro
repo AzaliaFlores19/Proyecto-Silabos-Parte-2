67b54868110f437fbc973ae5612ffee643dc8b51
AzaliaFlores19 <yamiflo19@unitec.edu>
2024-06-10 18:35:34 -0600

    -Cambios en el Gui

--- ProyectoSilabos.pro
+++ ProyectoSilabos.pro
@@ -9,11 +9,19 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    Usuario.cpp \
     main.cpp \
     cframe.cpp
 
 HEADERS += \
-    cframe.h
+    Arbol.h \
+    Estado.h \
+    Ingenieria.h \
+    Silabo.h \
+    Usuario.h \
+    cframe.h \
+    listaD.h \
+    nodoD.h
 
 FORMS += \
     cframe.ui
--- cframe.h
+++ cframe.h
@@ -2,6 +2,14 @@
 #define CFRAME_H
 
 #include <QMainWindow>
+#include <Usuario.h>
+#include <string>
+#include <listaD.h>
+#include <nodoD.h>
+#include <Estado.h>
+#include <Arbol.h>
+#include <QTableWidgetItem>
+using std::string;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class cframe; }
@@ -15,7 +23,73 @@ public:
     cframe(QWidget *parent = nullptr);
     ~cframe();
 
+
+
+
+private slots:
+
+       void on_btn_entregarM_2_clicked();
+       void on_btn_revision_2_clicked();
+       void on_btn_dashboardM_2_clicked();
+
+       //tab entrega
+       void on_tabWidget_currentChanged(int index);
+       void on_btn_silaboE_clicked();
+       void on_btn_archivoE_clicked();
+       void on_cb_facultadE_currentIndexChanged(int index);
+       void on_btn_closeE_clicked();
+
+       //tab revision
+       void on_Rcb_usuario_currentIndexChanged(int index);
+       void on_Rbtn_sesion_clicked();
+       void on_Rbtn_cerrar_clicked();
+       void on_Rbtn_cambiar_clicked();
+       void on_RTW_revision_cellClicked(int row, int column);
+
+       //tab board
+       void on_Bbtn_sesion_clicked();
+       void on_Bbtn_cerrar_clicked();
+       void on_Bbtn_proceso_clicked();
+       void on_Bbtn_aceptados_clicked();
+
+       //tab reload
+       void on_Dbtn_sesion_clicked();
+       void on_Dbtn_salir_clicked();
+       void on_DRTW_revision_cellClicked(int row, int column);
+
+
 private:
     Ui::cframe *ui;
+
+
+    bool loginDocente=false,loginRevision=false, revision2=false, loginBoard=false, loginCheck=false;
+    void limpiarEntrega();
+    void limpiarRevision();
+    void limpiarBoard();
+
+    void recorrerArbolParaTabla(Arbol *nodo, int &fila,nodoD<Usuario> *actD);
+    void recorrerArbolParaTable(Arbol *nodo, QTableWidget *tableWidget, const std::string &estado);
+    bool todo=false;
+
+    void pruebitaBotonesTab();
+
+    void cambiarSilabo(int id,QString pathActual);
+    void modificarDatosSilabo(Arbol *nodo,int id,QString pathNuevo);
+    string obtenerNombre(string);
+    bool cambiarPath=false, cambiarEstado=false;
+
+    listaD<Usuario> listaUsuarios;
+    nodoD<Usuario> *actD;
+    nodoD<Usuario> *ultD;
+
+    Arbol *actS;
+    Arbol *arbolSilabo  = new Arbol(nullptr);
+
+    int cantSilabos=0;
+    int id=0;
+
+    string claveDocente="teacher123", claveJefe="boss123",claveCoordinador="coordinator123",claveIEDD="what123",claveConsultor="advisor123",claveDecano="dean123",claveDirector="director123";
+
+    void mostrarDocente(Arbol *nodo,int fila,string numCuenta);
 };
 #endif // CFRAME_H
